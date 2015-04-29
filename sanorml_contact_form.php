25099e92bda74eb57dec3c3ec493311fada745d0
jvlatacc <john.vanlowe@g.austincc.edu>
2015-04-29 12:46:43 -0500

    Customize contact form files
    Configure email and add additional contact number field in html and php
    code.

--- sanorml_contact_form.php
+++ sanorml_contact_form.php
@@ -3,36 +3,58 @@
 $name = $_POST['name1'];
 $email = $_POST['email1'];
 $message = $_POST['message1'];
+$subject = $_POST['subject1'];
 $contact = $_POST['contact1'];
 $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
 // After sanitization Validation is performed
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 if (!preg_match("/^[0-9]{10}$/", $contact)) {
-echo "<span>* Please Fill Valid Contact No. *</span>";
+echo "Please put in a valid phone number (no dashes)”;
 } else {
-$subject = $name;
+
+$subjectCust = ‘Thank you for your message ‘.$name;
+$subjectOwner = ‘Website message from: ‘ . $name;
+
 // To send HTML mail, the Content-type header must be set.
-$headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From:' . $email. "\r\n"; // Sender's Email
-$headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
-$template = '<div style="padding:50px; color:white;">Hello ' . $name . ',<br/>'
-. '<br/>Thank you...! For Contacting Us.<br/><br/>'
-. 'Name:' . $name . '<br/>'
-. 'Email:' . $email . '<br/>'
-. 'Contact No:' . $contact . '<br/>'
-. 'Message:' . $message . '<br/><br/>'
-. 'This is a Contact Confirmation mail.'
-. '<br/>'
-. 'We Will contact You as soon as possible .</div>';
-$sendmessage = "<div style=\"background-color:#7E7E7E; color:white;\">" . $template . "</div>";
+$headers = ‘MIME-Version: 1.0′ . “\r\n”;
+$headers .= ‘Content-type: text/html; charset=iso-8859-1′ . “\r\n”;
+$headers .= ‘From:’ . $email. “\r\n”; // Sender’s Email
+$template2Customer = ‘Hello ‘ . $name . ‘,’
+. ‘Thank you for contacting us.’
+. ‘Your message to us: ‘
+. ‘Name:’ . $name . ”
+. ‘Email:’ . $email . ”
+. ‘Contact No:’ . $contact . ”
+. ‘Subject:’ . $subject . ”
+. ‘Message:’ . $message . ”
+. ‘This is a confirmation mail.’
+. ”
+. ‘We will contact you as soon as possible.’
+. ‘http://www.sanorml.org';
+
+$template2SiteOwner = ‘Website message from: ‘
+. ‘Name: ‘ . $name . ”
+. ‘Email: ‘ . $email . ”
+. ‘Contact No: ‘ . $contact . ”
+. ‘Subject: ‘ . $subject . ”
+. ‘Message: ‘ . $message . ”
+. ”
+. ‘This message was sent from: http://www.sanorml.org‘;
+
+$sendmessage2Cust = $template2Customer;
+$sendmessage2SiteOwner = $template2SiteOwner;
+
 // Message lines should not exceed 70 characters (PHP rule), so wrap it.
-$sendmessage = wordwrap($sendmessage, 70);
+$sendmessage2Cust = wordwrap($sendmessage2Cust, 70);
+$sendmessage2SiteOwner = wordwrap($sendmessage2SiteOwner, 70);
+
 // Send mail by PHP Mail Function.
-mail("receiver_email_id@abc.com", $subject, $sendmessage, $headers);
-echo "Your Query has been received, We will contact you soon.";
+mail($email, $subjectCust, $sendmessage2Cust, $headers);
+mail(“contact@sanorml.org”, $subjectOwner, $sendmessage2SiteOwner, $headers);
+
+echo “Your message has been received, we will contact you soon.”;
 }
 } else {
-echo "<span>* invalid email *</span>";
+echo “* invalid email *”;
 }
 ?>
